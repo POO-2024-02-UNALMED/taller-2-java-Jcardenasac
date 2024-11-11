4140d1e108e52ba14de47fd7861874b59e197512
Jcardenasac <jcardenasac@unal.edu.co>
2024-11-11 16:12:27 -0500

    final 1

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -6,6 +6,27 @@ public class Auto {
 	Asiento[] asientos;
 	Motor motor;
 	int registro;
-	String cantidadCreados;
-
+	static String cantidadCreados;
+	int cantidadAsientos(){
+		int cantidad = 0;
+		for(int i=0;i<this.asientos.length; i++) {
+			if(asientos[i]!= null) {
+				cantidad++;
+			}	
+		}
+		return cantidad;
+	}
+	String verificarIntegridad () {
+		if(this.registro != this.motor.registro) {
+			return "Las piezas del auto no son originales";
+		}
+		else{
+			for(int i=0;i<this.asientos.length; i++) {
+				if(asientos[i].registro != this.registro) {
+					return "Las piezas del auto no son originales";
+				}
+		}
+	}
+		return "Auto original";
+}
 }
--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -4,6 +4,12 @@ public class Motor {
 	int numeroCilindros;
 	String tipo;
 	int registro;
-	
-
+	void cambiarRegistro(int registronuevo) {
+		registro = registronuevo;
+	}
+    void asignarTipo (String nuevomotor) {
+    	if(nuevomotor == "electrico" || nuevomotor == "gasolina") {
+    		tipo = nuevomotor;
+    	}
+    }
 }
